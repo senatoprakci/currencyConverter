1f6473277f3be2640d3bb56fb85fddf205f11220
Sena Toprakcı <74680647+senatoprakci@users.noreply.github.com>
2023-07-10 17:39:49 +0300

    commit

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Sena Toprakcı on 10.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var cadLabel: UILabel!
+    @IBOutlet weak var chfLabel: UILabel!
+    @IBOutlet weak var gbpLabel: UILabel!
+    @IBOutlet weak var jpyLabel: UILabel!
+    @IBOutlet weak var usdLabel: UILabel!
+    @IBOutlet weak var tryLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func getRatesClicked(_ sender: Any) {
+        
+        // 1. Request & Session
+        // 2. Response & Data
+        // 3. Parsing & JSON Serialization
+        
+        
+        // 1.
+        let url = URL(string: "http://data.fixer.io/api/latest?access_key=069e1ae53b5a1646debc7447bcca1656")
+        
+        let session = URLSession.shared
+        
+        //Closure
+        let task = session.dataTask(with: url!) { data, response, error in
+            if error != nil {
+                
+                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert) //localizedDescription kullanıcının anlayacağı şekilde uyarı yazdırır.int bağlantısı yok vb gibi
+                
+                let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+                alert.addAction(okButton)
+                self.present(alert, animated: true)
+                
+                // 2.
+            } else {
+                if data != nil {
+                    
+                    do {
+                        let jsonResponse = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! Dictionary<String,Any>
+                        
+                        //ASYNC
+                        
+                        DispatchQueue.main.async {
+                            if let rates = jsonResponse["rates"] as? [String : Any] {
+                                
+                                if let cad = rates["CAD"] as? Double {
+                                    self.cadLabel.text = "CAD: \(cad)"
+                                }
+                                
+                                if let chf = rates["CHF"] as? Double {
+                                    self.chfLabel.text = "CHF: \(chf)"
+                                }
+                                
+                                if let gbp = rates["GBP"] as? Double {
+                                    self.gbpLabel.text = "GBP: \(gbp)"
+                                }
+                                
+                                if let jpy = rates["JPY"] as? Double {
+                                    self.jpyLabel.text = "JPY: \(jpy)"
+                                }
+                                
+                                if let usd = rates["USD"] as? Double {
+                                    self.usdLabel.text = "USD: \(usd)"
+                                }
+                                
+                                if let turkish = rates["TRY"] as? Double {
+                                    self.tryLabel.text = "TRY: \(turkish)"
+                                }
+
+                            }
+                                
+                        }
+                        
+                        
+                    } catch {
+                        print("error")
+                    }
+                    
+                }
+                
+                
+                
+                
+                
+                
+            }
+        }
+        task.resume()
+        
+    }
+}
+
